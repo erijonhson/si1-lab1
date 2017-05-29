62ad3c30be15aa254f5306cfe0f8293c787a58dc
erijonhson <eri.silva@ccc.ufcg.edu.br>
2017-05-28 22:08:10 -0300

    função para procurar número mínimo

--- find_max.py
+++ find_max.py
@@ -10,6 +10,21 @@ def find_max(int_list):
 			max_number = i
 	return max_number
 
+def find_min(int_list):
+	if int_list == []:
+		return None
+	min_number = int_list[0]
+	for i in int_list:
+		if min_number > i: 
+			min_number = i
+	return min_number
+
+assert None == find_min([])
+assert 1 == find_min([1,2,3])
+assert 4 == find_min([4,6,7])
+assert -3 == find_min([-1,-2,-3])
+
+
 assert None == find_max([])
 assert 3 == find_max([1,2,3])
 assert 7 == find_max([4,6,7])
